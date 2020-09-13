9aa6fb5d6131e3676353d7cb13c8e38ff817b959
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-14 01:37:10 +0530

    Chnage the folder structure and create laptop page

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ const server = http.createServer((req, res) => {
     res.end("This is the products");
   } else if (pathName === "/laptop" && id < laptopData.length) {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end("This is the laptop");
+    fs.readFile(`${__dirname}/templates/laptop.html`, "utf-8", (err, data) => {
+      const laptop = laptopData[id];
+      let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+      output = output.replace(/{%IMAGE%}/g, laptop.image);
+      output = output.replace(/{%PRICE%}/g, laptop.price);
+      output = output.replace(/{%SCREEN%}/g, laptop.screen);
+      output = output.replace(/{%CPU%}/g, laptop.cpu);
+      output = output.replace(/{%STORAGE%}/g, laptop.storage);
+      output = output.replace(/{%RAM%}/g, laptop.ram);
+      output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+      res.end(output);
+    });
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("URL not there");
