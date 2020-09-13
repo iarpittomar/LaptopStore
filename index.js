a9bcbcb8e4ceec6e20727d3ad168f8b4677445b7
Arpit Tomar <arpittomar7740@gmail.com>
2020-09-14 01:08:51 +0530

    basic structure

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
+const laptopData = JSON.parse(json);
 
-console.log(__dirname);
-console.log(json);
+const server = http.createServer((req, res) => {
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+
+  if (pathName === "/products" || pathName == "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the products");
+  } else if (pathName === "/laptop" && id < laptopData.length) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the laptop");
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("URL not there");
+  }
+});
+
+server.listen(1337, "127.0.0.1", () => {
+  console.log("server started litening");
+});
+
+console.log(laptopData);
